26be2bf17126786b792caf8b5d2cfef6478f3cd6
mkroeders <mkroeders@gmail.com>
2022-07-23 00:21:55 +0200

    Added developer authorization based upon account role

---
+++ src/Middleware/Authorization/Constraint/Account/DeveloperAccountAuthorizationConstraint.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace LessAbstractService\Middleware\Authorization\Constraint\Account;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception;
+use LessValueObject\Composite\ForeignReference;
+use Psr\Http\Message\ServerRequestInterface;
+
+final class DeveloperAccountAuthorizationConstraint extends AbstractAccountAuthorizationConstraint
+{
+    public function __construct(private readonly Connection $connection)
+    {}
+
+    /**
+     * @throws Exception
+     */
+    protected function isIdentityAllowed(ServerRequestInterface $request, ForeignReference $identity): bool
+    {
+        $builder = $this->connection->createQueryBuilder();
+        $role = $builder
+            ->select('role')
+            ->from('account_role')
+            ->andWhere('account_type = :account_type')
+            ->setParameter('account_type', $identity->type)
+            ->andWhere('account_id = :account_id')
+            ->setParameter('account_id', $identity->id)
+            ->fetchOne();
+
+        return $role === 'developer';
+    }
+}
