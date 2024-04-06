3f9b3d0c0d3a1b9221aaf4fec398c66e183851c3
mkroeders <mkroeders@gmail.com>
2024-04-06 15:51:07 +0200

    Need to use json_value

--- docs/database/event_store.sql
+++ docs/database/event_store.sql
@@ -1,7 +1,5 @@
 ALTER TABLE event_store
-    add column v_target_id varchar(36) GENERATED ALWAYS as (JSON_UNQUOTE(JSON_EXTRACT(parameters, '$.id'))) STORED,
-    add column v_header_ip varchar(36) GENERATED ALWAYS as (JSON_UNQUOTE(JSON_EXTRACT(headers, '$.ip'))) STORED;
-
-ALTER TABLE event_store
+    add column v_target_id varchar(36) GENERATED ALWAYS as (json_value(parameters, '$.id')) STORED,
+    add column v_header_ip varchar(36) GENERATED ALWAYS as (json_value(headers, '$.ip')) STORED,
     add index (v_target_id, occurred_on),
     add index (v_header_ip, occurred_on);
