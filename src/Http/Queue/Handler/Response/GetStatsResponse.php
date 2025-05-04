a14ce886d48bd17edad65a6bd31bb4900760ffec
mkroeders <mkroeders@gmail.com>
2025-05-04 12:30:32 +0200

    Added queue get stats

---
+++ src/Http/Queue/Handler/Response/GetStatsResponse.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace LesAbstractService\Http\Queue\Handler\Response;
+
+use LesValueObject\Number\Int\Unsigned;
+use LesValueObject\Composite\AbstractCompositeValueObject;
+
+/**
+ * @psalm-immutable
+ */
+final class GetStatsResponse extends AbstractCompositeValueObject
+{
+    public function __construct(
+        public readonly Unsigned $processable,
+        public readonly Unsigned $processing,
+        public readonly Unsigned $buried,
+    ) {}
+}
