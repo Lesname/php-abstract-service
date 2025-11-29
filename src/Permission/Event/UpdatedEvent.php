a5d9d244c7e881b43108e2186d1818cf9512f8fc
mkroeders <mkroeders@gmail.com>
2025-11-29 22:02:00 +0100

    EventActionHelper was dropped due to issues with not in php supproted

--- src/Permission/Event/UpdatedEvent.php
+++ src/Permission/Event/UpdatedEvent.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace LesAbstractService\Permission\Event;
 
+use LesDomain\Event\Property\Action;
 use LesDomain\Event\Property\Headers;
 use LesDomain\Event\AbstractAggregateEvent;
-use LesDomain\Event\Helper\EventActionHelper;
 use LesValueObject\Number\Int\Date\MilliTimestamp;
 use LesValueObject\String\Format\Resource\Identifier;
 use LesAbstractService\Permission\Model\Attributes\Flags;
@@ -16,8 +16,11 @@ use LesAbstractService\Permission\Model\Attributes\Flags;
  */
 final class UpdatedEvent extends AbstractAggregateEvent
 {
-    use EventActionHelper;
     use PermissionEvent;
+
+    public Action $action {
+        get => new Action('updated');
+    }
 
     public function __construct(
         Identifier $id,
